751c617cfdaaa50f6d1241a1ff3360828cc25090
MayaS12 <71437198+MayaS12@users.noreply.github.com>
2022-09-20 20:06:08 +0530

    Add files via upload

---
+++ navigation/stackNav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomeScreen from "../screens/Home";
+import TopTabNav from "./topTabNav";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const AppStackNavigation = createStackNavigator();
+export default function StackNav() {
+  return (
+    <AppStackNavigation.Navigator
+      screenOptions={() => ({
+        headerShown: false,
+      })}
+    >
+      <AppStackNavigation.Screen name="Home" component={HomeScreen} />
+    </AppStackNavigation.Navigator>
+  );
+}
